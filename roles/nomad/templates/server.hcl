c20a7fc8c463ad5eeb48b0633a850e233da359d3
AlxZakrzewski <alx.zakrzewski@gmail.com>
2022-01-29 23:05:27 +0100

    Adjust Nomad configuration

--- roles/nomad/templates/server.hcl
+++ roles/nomad/templates/server.hcl
@@ -1,10 +1,6 @@
 server {
   enabled = true
   bootstrap_expect = 3
-
-  server_join {
-      retry_join = ["127.0.0.1:4648"]
-  }
 }
 consul {
     address = "127.0.0.1:8500"
--- vagrant/Centos7/firewall.sh
+++ vagrant/Centos7/firewall.sh
@@ -3,19 +3,11 @@
 # Add firewall rules
 
 # Nomad
-sudo firewall-cmd --zone=public --permanent --add-port=4646/tcp
-sudo firewall-cmd --zone=public --permanent --add-port=4647/tcp
-sudo firewall-cmd --zone=public --permanent --add-port=4648/tcp
+sudo firewall-cmd --zone=public --permanent --add-port={4646,4647,4648}/tcp
 sudo firewall-cmd --zone=public --permanent --add-port=4648/udp
 # Consul
-sudo firewall-cmd --zone=public --permanent --add-port=8600/tcp
-sudo firewall-cmd --zone=public --permanent --add-port=8600/udp
-sudo firewall-cmd --zone=public --permanent --add-port=8500/tcp
-sudo firewall-cmd --zone=public --permanent --add-port=8301/tcp
-sudo firewall-cmd --zone=public --permanent --add-port=8301/udp
-sudo firewall-cmd --zone=public --permanent --add-port=8302/tcp
-sudo firewall-cmd --zone=public --permanent --add-port=8302/udp
-sudo firewall-cmd --zone=public --permanent --add-port=8300/tcp
+sudo firewall-cmd --zone=public --permanent --add-port={8300,8301,8302,8500,8600}/tcp
 sudo firewall-cmd --zone=public --permanent --add-port=21000-21255/tcp
+sudo firewall-cmd --zone=public --permanent --add-port={8301,8302,8600}/udp
 
 sudo firewall-cmd --reload
