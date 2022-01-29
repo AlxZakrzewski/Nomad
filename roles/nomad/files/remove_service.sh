a44b71aa4df3f4ae0e21daad817bcaf9d52ba90a
AlxZakrzewski <alx.zakrzewski@gmail.com>
2022-01-30 00:04:46 +0100

    Fix remove_service.sh

--- roles/nomad/files/remove_service.sh
+++ roles/nomad/files/remove_service.sh
@@ -3,8 +3,8 @@
 sudo systemctl stop nomad.service
 sudo systemctl disable nomad.service
 sudo rm /etc/systemd/system/nomad.service
-sudo rm /etc/systemd/system/nomad.service
-sudo rm /usr/lib/systemd/system/nomad.service
-sudo rm /usr/lib/systemd/system/nomad.service
+sudo rm -rf /etc/systemd/system/nomad.service
+sudo rm -rf /usr/lib/systemd/system/nomad.service
+sudo rm -rf /usr/lib/systemd/system/nomad.service
 sudo systemctl daemon-reload
 sudo systemctl reset-failed
